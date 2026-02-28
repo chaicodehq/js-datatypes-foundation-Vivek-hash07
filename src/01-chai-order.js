3323aa92d8207e5422818a3f2b8b3a6893da9250
Vivek Sarvaiya <viveksarvaiya1873@gmail.com>
2026-02-28 03:49:26 +0000

    test 1 completed.

--- src/01-chai-order.js
+++ src/01-chai-order.js
@@ -47,20 +47,41 @@
  */
 export function getChaiOrderLength(order) {
   // Your code here
+  if(typeof order !== "string")return -1;
+  return order.trim().length
 }
 
 export function shoutChaiOrder(order) {
   // Your code here
+  if(typeof order !== "string")return "";
+  if(order.trim() === "" || order.trim === isNaN) return "";
+  return order.trim().toUpperCase()
 }
 
 export function whisperChaiOrder(order) {
   // Your code here
+  if(typeof order !== "string")return "";
+  if(order.trim() === "" || order.trim === isNaN) return "";
+  return order.trim().toLowerCase()
+
 }
 
 export function hasSpecialIngredient(order, ingredient) {
   // Your code here
+  if(typeof order !== "string" || typeof ingredient !== "string") return false;
+  order = order.trim().toLowerCase()
+  ingredient = ingredient.trim().toLowerCase()
+  return order.includes(ingredient)
 }
 
 export function getFirstAndLastChar(order) {
-  // Your code here
+  if (typeof order !== "string") return null;
+
+  const trimmed = order.trim();
+  if (trimmed === "") return null;
+
+  return {
+    first: trimmed.charAt(0),
+    last: trimmed.charAt(trimmed.length - 1)
+  };
 }
